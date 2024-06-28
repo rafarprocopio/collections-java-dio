c3df7584667ae1e63efcc5f389aa3517241f87ee
Rafaely RP <rafarprocopio@gmail.com>
2024-06-28 13:57:31 -0300

    first commit

---
+++ src/ListaTarefa.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //Atributo
+    private List <Tarefa> tarefalList;
+
+    public ListaTarefa() {
+        this.tarefalList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefalList.add(new Tarefa(descricao));
+        }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> TarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefalList) {
+            if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                    TarefasParaRemover.add(t);
+            }
+        }
+        tarefalList.removeAll(TarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefalList.size();
+        }
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefalList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa - new ListaTarefa() {
+            System.out.println("o número total de tarefas na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+    }
+
+    
+}
---
+++ src/Tarefa.java
@@ -0,0 +1,13 @@
+public class Tarefa {
+    //Atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    
+}
