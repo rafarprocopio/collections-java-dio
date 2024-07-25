70010acb0219ba6aae11533f87645f7ffd032369
Rafaely RP <rafarprocopio@gmail.com>
2024-07-25 13:23:30 -0300

    alsterações exercícios de pesquisaList

--- src/PesquisaList/SomaDeNumeros/SomaNumeros.java
+++ src/PesquisaList/SomaDeNumeros/SomaNumeros.java
@@ -3,9 +3,11 @@ package PesquisaList.SomaDeNumeros;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
+
 public class SomaNumeros {
 
-    private List<numero> inteiroslist;
+    private List <Integer> numerolist;
 
     //declaração feita automatica através do Generate Constructor. Depois substituido por vazio e inserido uma new ArrayList Vazio tbm selecionado automaticamente
     public SomaNumeros() {
@@ -13,6 +15,29 @@ public class SomaNumeros {
     }
     
     public void adicionarNumero(int numero) {
-        numerolist.add(new Numero(numero));
+        this.numerolist.add(numero);
+    }
+
+    public int calcularSoma() {
+        int soma = 0;
+        for (Integer numero : numerolist) {
+            soma += numero;
+        }
+        return soma;
+    }
+
+    public int encontrarMaiorNumero(){
+        int maiorNumero = 0;
+        if (!numerolist.isEmpty()) {
+            for (Integer numero : numerolist) {
+                if (numero >= maiorNumero) {
+                    maiorNumero = numero;
+                }
+            }
+            return maiorNumero;
+        } else {
+            throw new RuntimeException("A lista está vazia");
+        }
+
     }
 }
