e6e4398d994297f28ea71bd21226054e46598112
Rafaely RP <rafarprocopio@gmail.com>
2024-07-28 16:05:11 -0300

    alterações exercicio SomaNumeros - PesquisaList

--- src/PesquisaList/SomaDeNumeros/SomaNumeros.java
+++ src/PesquisaList/SomaDeNumeros/SomaNumeros.java
@@ -61,28 +61,22 @@ public class SomaNumeros {
       }
 
     public static void main(String[] args) {
-        // Criando uma instância da classe SomaNumeros
     SomaNumeros somaNumeros = new SomaNumeros();
 
-    // Adicionando números à lista
-    somaNumeros.adicionarNumero(5);
+    somaNumeros.adicionarNumero(7);
+    somaNumeros.adicionarNumero(3);
     somaNumeros.adicionarNumero(0);
-    somaNumeros.adicionarNumero(0);
-    somaNumeros.adicionarNumero(-2);
+    somaNumeros.adicionarNumero(-4);
     somaNumeros.adicionarNumero(10);
     somaNumeros.adicionarNumero(8);
 
-    // Exibindo a lista de números adicionados
     System.out.println("Números adicionados:");    
     somaNumeros.exibirNumeros();
 
-    // Calculando e exibindo a soma dos números na lista
     System.out.println("Soma dos números = " + somaNumeros.calcularSoma());
 
-    // Encontrando e exibindo o maior número na lista
     System.out.println("Maior número = " + somaNumeros.encontrarMaiorNumero());
 
-    // Encontrando e exibindo o menor número na lista
     System.out.println("Menor número = " + somaNumeros.encontrarMenorNumero());
   }
     
